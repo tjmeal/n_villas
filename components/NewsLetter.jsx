4985212ad62113de062366c45353e10e1c0a6997
tjmeal <themispagonis@gmail.com>
2024-01-16 14:38:26 +0200

    update

--- components/NewsLetter.jsx
+++ components/NewsLetter.jsx
@@ -1,31 +1,33 @@
 import React from "react";
 
 const NewsLetter = () => {
-  return (
-    <div className="container">
-      <div className="row">
-        <div className="col-lg-7 mx-auto">
-          <div className="cta">
-            <h4>For Recent Update, News.</h4>
-            <p>
-              We helps businesses customize, automate and scale up their ad
-              production and delivery.
-            </p>
-            <form action="GET" className="flex align-items-center">
-              <input
-                type="text"
-                className="form-control"
-                placeholder="Enter Your Email"
-              />
-              <button className="btn d-block d-md-inline-block w-md-auto w-100">
-                Subscribe
-              </button>
-            </form>
-          </div>
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col-lg-7 mx-auto">
+                    <div className="cta">
+                        <h4>For Recent Update, News.</h4>
+                        <p>
+                            We helps businesses customize, automate and scale up their ad
+                            production and delivery.
+                        </p>
+                        <form action="GET" className="flex align-items-center">
+                            <label htmlFor="email">Email: </label>
+                            <input
+                                id="email"
+                                type="text"
+                                className="form-control"
+                                placeholder="Enter Your Email"
+                            />
+                            <button className="btn d-block d-md-inline-block w-md-auto w-100">
+                                Subscribe
+                            </button>
+                        </form>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default NewsLetter;
