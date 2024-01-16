e1c867e396df0759f133f12394b6e2b169ba7b72
tjmeal <themispagonis@gmail.com>
2024-01-16 14:21:04 +0200

    update

--- app/layout.js
+++ app/layout.js
@@ -30,8 +30,8 @@ export default function RootLayout({children}) {
         <Navbar />
         <main>{children}</main>
         <Footer />
-        <LogIn />
-        <CreateAccount />
+        {/*<LogIn />*/}
+        {/*<CreateAccount />*/}
         {/*<ForgetPassword />*/}
         {/*<Otp />*/}
         {/*<NewPassword />*/}
