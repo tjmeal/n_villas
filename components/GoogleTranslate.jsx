3737cb6ae0c25a518d89a7c7c18556935661b2ae
tjmeal <themispagonis@gmail.com>
2024-09-18 13:36:04 +0300

    update

--- components/GoogleTranslate.jsx
+++ components/GoogleTranslate.jsx
@@ -72,8 +72,9 @@ const Translate = () => {
                 onSelect={(e, m, evt) => langChange(e, m, evt)}
                 placeholder="Lang"
             />
-            <Link title={" "} href="https://drive.google.com/file/d/12oKmEfFyzv_4Wiz0bfXan3on1WFTLq4m/view?usp=sharing">
+            <Link href="https://drive.google.com/file/d/12oKmEfFyzv_4Wiz0bfXan3on1WFTLq4m/view?usp=sharing">
                 <div className="translateImg">
+                    <title>File</title>
                     {selected === '/auto/el' ? ( // Display Greek image if selected language is 'el'
                         <Image src={greekImage} alt="Greek" className="imge"/>
                     ) : (
